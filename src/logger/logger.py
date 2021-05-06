f276a5c5fe0459fa45fc584316dabfdbe12a3c8b
Youngbin Han <youngbin.han@cloudmt.co.kr>
2021-05-06 15:11:45 +0900

    디버그 로깅 테스트

--- src/logger/logger.py
+++ src/logger/logger.py
@@ -3,5 +3,6 @@ import os
 
 from src.logger.logger_tool import get_default_logger
 
-log_level = logging.INFO if os.getenv('STAGE') == 'prod' else logging.DEBUG
+# log_level = logging.INFO if os.getenv('STAGE') == 'prod' else logging.DEBUG
+log_level = logging.DEBUG
 LOGGER = get_default_logger(__name__, level=log_level)
