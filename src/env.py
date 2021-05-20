d17a2abf81291f14a1bda01d36379d5922aca475
Youngbin Han <youngbin.han@cloudmt.co.kr>
2021-05-20 11:36:52 +0900

    refresh token 명령 인자로 전달 가능하도록 수정

--- src/env.py
+++ src/env.py
@@ -26,7 +26,7 @@ class AzurePartnerCenterEnv:
         parser.add_argument('-f', '--file', action='store_true', help='.env파일을 통한 환경변수 등록')
         parser.add_argument('--database-password', type=str, help='')
         parser.add_argument('--mfa-auth-issue', action='store_true', help='MFA 인증으로 토큰 발급하여 저장')
-        parser.add_argument('--mfa-auth', action='store_true', help='MFA 인증으로 발급한 토큰으로 인증')
+        parser.add_argument('--mfa-auth', action=str, help='MFA 인증으로 발급한 토큰으로 인증')
         parser.add_argument('--app-secret', type=str, help='')
         parser.add_argument('--daily-usage', action='store_true', help='일별 사용량 수집')
         parser.add_argument('--daily-usage-update', action='store_true', help='일별 사용량 수집에 대한 과거 데이터 업데이트')
@@ -77,9 +77,21 @@ class AzurePartnerCenterEnv:
         return self.__access_token
 
     def refresh_token(self) -> None:
-        self.__get_token()
+        if self.mfa_auth:
+            self.__get_token(
+                resource="https://api.partnercenter.microsoft.com",
+                grant_type="refresh_token",
+                refresh_token=self.mfa_auth,
+                scope="openid"
+            )
+        else:
+            self.__get_token()
 
-    def __get_token(self, resource: str = 'https://graph.windows.net') -> None:
+    def __get_token(self, 
+        resource: str = 'https://graph.windows.net',
+        grant_type: str = "client_credentials",
+        refresh_token: str = None,
+        scope: str = None) -> None:
         """
         Azure Partner Center API를 호출하기 위한 auth token 발급
         :param resource:
@@ -94,8 +106,14 @@ class AzurePartnerCenterEnv:
             'resource': resource,
             'client_id': self.appid,
             'client_secret': self.secret,
-            'grant_type': 'client_credentials'
+            'grant_type': grant_type
         }
+
+        if refresh_token is not None:
+            data["refresh_token"] = refresh_token
+        if scope is not None:
+            data["scope"] = scope
+
         r = requests.post(url=url, data=data, headers=headers)
         r.raise_for_status()
         # TODO: 에러처리
